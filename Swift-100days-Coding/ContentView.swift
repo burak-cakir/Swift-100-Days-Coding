2c8d4a064db621246682cc7952f5ba3af0fe4087
burak-cakir <burakcakir@hotmail.com.tr>
2021-12-25 19:33:53 +0300

    Initial Commit

---
+++ Swift-100days-Coding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-100days-Coding
+//
+//  Created by burak cakir on 25.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-100days-Coding/Swift_100days_CodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_100days_CodingApp.swift
+//  Swift-100days-Coding
+//
+//  Created by burak cakir on 25.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Swift_100days_CodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
