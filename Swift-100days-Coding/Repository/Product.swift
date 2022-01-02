9ab6094441cafb2ad43a430b206a7a5a7bc699e1
burak-cakir <burakcakir@hotmail.com.tr>
2022-01-02 13:10:49 +0300

    test

--- Swift-100days-Coding/Repository/Product.swift
+++ Swift-100days-Coding/Repository/Product.swift
@@ -12,6 +12,7 @@ struct Product {
     var productName : String
     var productDescription : String
     var productunitInStok : Int
+    
     var productprice : Int = 0
     var productisActive : Bool = true
     var productcategory : productCategory = productCategory(categoryname: "...", categortDescription: "...")
